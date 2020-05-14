203fd15b81a009dc776a3749f46698b3c5ff64a8
Andres Fabian Patiño Bermudez <97bermudez.andres@gmail.com>
2020-05-14 15:54:20 -0500

    agregado nuevas escenas y cambios a ellas

---
+++ Assets/Scripts/CambiarEscenas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscenas : MonoBehaviour
+{
+    public static CambiarEscenas instance = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/generadorLlegada.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generadorLlegada : MonoBehaviour
+{
+    public int marcador = 0, repeticiones = 5, segundos;
+    public gameObject Llegada, segunderos;
+    GameObject llegadaActual;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
